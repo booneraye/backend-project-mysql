dc00a5933b0dbde6de10b01c3e80a27d0e364e0f
Boone Raye Flores <booneraye@gmail.com>
2022-04-26 09:53:16 +0800

    added authentication method

--- routes/users.js
+++ routes/users.js
@@ -25,57 +25,70 @@ const checkHashedPassword = (password, hased) => {
   return matched;
 };
 
-router.route("/login").post((req, res) => {
-  const { username, password } = req.body;
-  console.log(req.body);
-  let sql = `SELECT username, email_address, password, access_level FROM users WHERE username="${username}"`;
-  con.query(sql, function (err, result) {
-    if (err) {
+const authenticateAccount = (res, admin_username, actions) => {
+  //check if the username has admin priviledges
+  let auth_sql = `SELECT access_level FROM users WHERE username="${admin_username}" AND access_level="administrator"`;
+  con.query(auth_sql, function (err, auth_result) {
+    if (auth_result.length != 0) {
+      actions();
+    } else {
       res.send({
         status: "failed",
-        message: err,
+        message: "You are not authorized to access this part",
       });
-    } else {
-      if (
-        // result[0].password &&
-        checkHashedPassword(password, result[0].password)
-      ) {
-        let data = result.map((r) => ({
-          username: r.username,
-          email_address: r.email_address,
-          access_level: r.access_level,
-          status: "success",
-          message: "Username and Password matched!",
-          token: uuid.v4(),
-        }));
+    }
+  });
+};
 
-        res.send(data[0]);
-      } else {
-        res.send({
-          status: "failed",
-          message: "Username and Password not matched!",
-        });
-      }
+router.route("/login").post((req, res) => {
+  const { username, password } = req.body;
+
+  let sql = `SELECT username, email_address, password, access_level FROM users WHERE username="${username}"`;
+  con.query(sql, function (err, result) {
+    if (
+      // result[0].password &&
+      checkHashedPassword(password, result[0].password)
+    ) {
+      let data = result.map((r) => ({
+        username: r.username,
+        email_address: r.email_address,
+        access_level: r.access_level,
+        status: "success",
+        message: "Username and Password matched!",
+        token: uuid.v4(),
+      }));
+
+      res.send(data[0]);
+    } else {
+      res.send({
+        status: "failed",
+        message: "Username and Password not matched!",
+      });
     }
   });
 });
 
 router.route("/users").post((req, res) => {
-  let sql = `SELECT id, first_name, last_name, address, post_code, contact_phone_number, email_address, username, access_level FROM users`;
-  con.query(sql, function (err, result) {
-    if (err) {
-      res.send({
-        status: "failed",
-        message: err,
-      });
-    } else {
-      res.send(result);
-    }
+  const { admin_username } = req.body;
+
+  authenticateAccount(res, admin_username, () => {
+    let sql = `SELECT id, first_name, last_name, address, post_code, contact_phone_number, email_address, username, access_level FROM users`;
+    con.query(sql, function (err, result) {
+      if (err) {
+        res.send({
+          status: "failed",
+          message: err,
+        });
+      } else {
+        res.send(result);
+      }
+    });
   });
 });
 
 router.route("/add_user").post((req, res) => {
   const {
+    admin_username,
     first_name,
     last_name,
     address,
@@ -87,7 +100,8 @@ router.route("/add_user").post((req, res) => {
     access_level,
   } = req.body;
 
-  let sql = `INSERT INTO users 
+  authenticateAccount(res, admin_username, () => {
+    let sql = `INSERT INTO users 
   (first_name, last_name, address, post_code, contact_phone_number, email_address, username, password, access_level) 
   VALUES 
   ("${first_name}", 
@@ -100,20 +114,22 @@ router.route("/add_user").post((req, res) => {
   "${getHashedPassword(password)}", 
   "${access_level}")`;
 
-  con.query(sql, function (err, result) {
-    if (err) {
-      res.send({
-        status: "failed",
-        message: err,
-      });
-    } else {
-      res.send(result);
-    }
+    con.query(sql, function (err, result) {
+      if (err) {
+        res.send({
+          status: "failed",
+          message: err,
+        });
+      } else {
+        res.send(result);
+      }
+    });
   });
 });
 
 router.route("/update_user").post((req, res) => {
   const {
+    admin_username,
     id,
     first_name,
     last_name,
@@ -126,56 +142,63 @@ router.route("/update_user").post((req, res) => {
     access_level,
   } = req.body;
 
-  let sql = `UPDATE users SET first_name="${first_name}", 
-  last_name="${last_name}", 
-  address="${address}", 
-  post_code="${post_code}", 
-  contact_phone_number="${contact_phone_number}", 
-  email_address="${email_address}", 
-  username="${username}", 
-  password="${getHashedPassword(password)}", 
-  access_level="${access_level}" WHERE id="${id}" AND username="${username}"`;
-  con.query(sql, function (err, result) {
-    if (err) {
-      res.send({
-        status: "failed",
-        message: err,
-      });
-    } else {
-      res.send(result);
-    }
+  authenticateAccount(res, admin_username, () => {
+    let sql = `UPDATE users SET first_name="${first_name}", 
+    last_name="${last_name}", 
+    address="${address}", 
+    post_code="${post_code}", 
+    contact_phone_number="${contact_phone_number}", 
+    email_address="${email_address}", 
+    username="${username}", 
+    password="${getHashedPassword(password)}", 
+    access_level="${access_level}" WHERE id="${id}" AND username="${username}"`;
+    con.query(sql, function (err, result) {
+      if (err) {
+        res.send({
+          status: "failed",
+          message: err,
+        });
+      } else {
+        res.send(result);
+      }
+    });
   });
 });
 
 router.route("/delete_user").post((req, res) => {
-  const { id, username } = req.body;
+  const { admin_username, id, username } = req.body;
 
-  let sql = `DELETE FROM users WHERE id="${id}" AND username="${username}"`;
-  con.query(sql, function (err, result) {
-    if (err) {
-      res.send({
-        status: "failed",
-        message: err,
-      });
-    } else {
-      res.send(result);
-    }
+  authenticateAccount(res, admin_username, () => {
+    let sql = `DELETE FROM users WHERE id="${id}" AND username="${username}"`;
+    con.query(sql, function (err, result) {
+      if (err) {
+        res.send({
+          status: "failed",
+          message: err,
+        });
+      } else {
+        res.send(result);
+      }
+    });
   });
 });
 
 router.route("/multiple_delete_users").post((req, res) => {
-  const { users } = req.body;
-  let usersArray = users.map((u) => [u.id, u.username]);
+  const { admin_username, users } = req.body;
 
-  let sql = `DELETE FROM users WHERE (id, username) IN (?)`;
-  con.query(sql, [usersArray], function (err, result) {
-    if (err) {
-      res.send({
-        status: "failed",
-        message: err,
-      });
-    }
-    res.send(result);
+  authenticateAccount(res, admin_username, () => {
+    let usersArray = users.map((u) => [u.id, u.username]);
+
+    let sql = `DELETE FROM users WHERE (id, username) IN (?)`;
+    con.query(sql, [usersArray], function (err, result) {
+      if (err) {
+        res.send({
+          status: "failed",
+          message: err,
+        });
+      }
+      res.send(result);
+    });
   });
 });
 
